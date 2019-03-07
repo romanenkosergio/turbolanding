8771ba85593dba3b8d352ff61c36489fab9f8dca
Romanenko Sergio <sergromanenko97@gmail.com>
2019-03-07 16:41:16 +0200

    Сверстан первый экран и немного адаптива

--- app/js/common.js
+++ app/js/common.js
@@ -1,29 +1,7 @@
 $(function() {
-    $(".services__carosel").slick({
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        speed: 300,
-        arrows: false,
-        dots: true,
-        draggable: false,
-        responsive: [{
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    })
+    $(".header__arrow").on('click', function() {
+        $(this).toggleClass('header__arrow_reverse');
+        $(".header-menu__tablet").toggleClass('header-menu__tablet_active');
+    });
+
 });
